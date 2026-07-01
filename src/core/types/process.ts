4916fa228c1861d4688789bd6cb9c4ed3f8f6277
Ángela Zhouling Izquierdo Padrón <alu0101480442@ull.edu.es>
2026-07-01 15:05:41 +0100

    Eliminar v-01 para volver a crear de 0

--- src/core/types/process.ts
+++
@@ -1,7 +0,0 @@
-// Proceso de entrada tal como lo proporciona el usuario
-export interface Process {
-  readonly id: string;
-  readonly arrival_time: number;  // entero >= 0
-  readonly burst_time: number;    // entero > 0
-  readonly priority?: number;     // opcional; menor número = mayor prioridad
-}
